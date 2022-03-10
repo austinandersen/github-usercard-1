8282f0c4349f3f3780de757cbc0691e81745c6c7
austin andersen <austinjamesgf@gmail.com>
2022-03-10 13:10:58 -0800

    complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,12 @@
+import axios from "axios";
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -16,20 +20,6 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
-
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
-
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
-
-const followersArray = [];
-
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -49,12 +39,93 @@ const followersArray = [];
       </div>
     </div>
 */
-
 /*
-  List of LS Instructors Github username's:
+  STEP 5: 
+    Using that array, iterate over it, requesting data for each user, creating a new card for each user, and adding that card to the DOM.
     tetondan
     dustinmyers
     justsml
     luishrd
     bigknell
 */
+
+//ok so I need a for each this and request data (axios.get)
+//create new card (call gitCard) appended to DOM
+
+// followersArray.forEach(userLogin => {
+//  axios.get(`https://api.github.com/users/${userLogin}`)
+//    .then(res => {
+//       const newCard = gitCard(res.data)
+//       cardClass.appendChild(newCard)
+//   .catch(err => {
+//     console.error(err);
+//   })
+//   .finally(() => {
+//     console.log("IT'S WORKING, IT'S WORKING!");
+//   })
+//  })
+// })
+
+const followersArray = ["tetondan", "dustinmyers", "justsml", "luishrd", "bigknell"];
+
+
+for (let i = 0; i < followersArray.length; i++) {
+  friendCard(followersArray[i])
+}
+
+function friendCard(username) {
+  axios.get(`https://api.github.com/users/${username}`)
+  .then(res => {
+    document.querySelector('.cards').appendChild(gitCard(res.data))
+  }).catch(err => {
+    console.error(err);
+  })
+}
+
+
+function gitCard(gitInfo) {
+  const hubCard = document.createElement("div")
+  const img = document.createElement("img")
+  const cardInfo = document.createElement("div")
+  const userName = document.createElement("h3")
+  const userLogin = document.createElement("p")
+  const userLocation = document.createElement("p")
+  const userProfile = document.createElement("p")
+  const address = document.createElement("a")
+  const userFollowers = document.createElement("p")
+  const userFollowing = document.createElement("p")
+  const userBio = document.createElement("p")
+
+  img.src = gitInfo.avatar_url
+  userName.textContent = gitInfo.name
+  userLogin.textContent =  gitInfo.login
+  userLocation.textContent =  gitInfo.location
+  userProfile.textContent = "Profile:"
+  address.textContent = "Link to profile"
+  address.href = gitInfo.html_url
+  userFollowers.textContent = `Followers: ${gitInfo.followers}`
+  userFollowing.textContent = `Following: ${gitInfo.following}`
+  userBio.textContent = `Bio: ${gitInfo.bio}`
+
+  hubCard.classList.add("card")
+  cardInfo.classList.add("card-info")
+  userName.classList.add("name")
+  userLogin.classList.add("username")
+  
+
+  hubCard.appendChild(img)
+  hubCard.appendChild(cardInfo)
+  cardInfo.appendChild(userName)
+  cardInfo.appendChild(userLogin)
+  cardInfo.appendChild(userLocation)
+  cardInfo.appendChild(userProfile)
+  userProfile.appendChild(address)
+  cardInfo.appendChild(userFollowers)
+  cardInfo.appendChild(userFollowing)
+  cardInfo.appendChild(userBio)
+
+  return hubCard;
+}
+
+
+
